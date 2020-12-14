027c62f127735274734864862b733d7fa4185fed
lucas-goldner <lucas.goldner@googlemail.com>
2020-12-14 22:46:15 +0100

    Initial Commit

---
+++ JoggingTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JoggingTracker
+//
+//  Created by Lucas Goldner on 14.12.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
