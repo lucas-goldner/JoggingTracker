d4c38e08f971c1d1b9b964d96a84b352289a4216
lucas-goldner <lucas.goldner@googlemail.com>
2020-12-14 23:59:03 +0100

    Navigationcontroller Setup

---
+++ JoggingTracker/DebugViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DebugViewController.swift
+//  JoggingTracker
+//
+//  Created by Lucas Goldner on 14.12.20.
+//
+
+import UIKit
+
+class DebugViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- JoggingTracker/ViewController.swift
+++ JoggingTracker/ViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var GoRunButton: UIButton!
+    @IBOutlet weak var OldRunsButton: UIButton!
+    @IBAction func DebugButton(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
