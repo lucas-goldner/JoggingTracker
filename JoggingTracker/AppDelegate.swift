e04275eb6b410a324dc27fa204450bfcc6163834
lucas-goldner <lucas.goldner@googlemail.com>
2020-12-15 22:35:24 +0100

    Added Locationaccess

--- JoggingTracker/AppDelegate.swift
+++ JoggingTracker/AppDelegate.swift
@@ -13,6 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let locationManager = LocationManager.shared
+        locationManager.requestWhenInUseAuthorization()
         // Override point for customization after application launch.
         return true
     }
