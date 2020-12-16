347156e2f341cf8c242c785b5ff34f8eddd7fdb3
lucas-goldner <lucas.goldner@googlemail.com>
2020-12-16 20:49:50 +0100

    Added Roadmap and the labels

--- JoggingTracker/RunsListViewController.swift
+++ JoggingTracker/RunsListViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class RunsListViewController: UIViewController {
+    @IBOutlet weak var TimeView: UITextView!
+    @IBOutlet weak var PaceView: UITextView!
+    @IBOutlet weak var DateView: UITextView!
     var run: Run!
 
     override func viewDidLoad() {
@@ -17,7 +20,38 @@ class RunsListViewController: UIViewController {
     }
     
     private func configureView() {
+        let distance = Measurement(value: run.distance, unit: UnitLength.meters)
+        let seconds = Int(run.duration)
+        let formattedDistance = FormatDisplay.distance(distance)
+        let formattedDate = FormatDisplay.date(run.timestamp)
+        let formattedTime = FormatDisplay.time(seconds)
+        let formattedPace = FormatDisplay.pace(distance: distance,
+                                               seconds: seconds,
+                                               outputUnit: UnitSpeed.minutesPerMile)
+        
+  
+        DateView.text = formattedDate
+        TimeView.text = "\(formattedTime)"
+        PaceView.text = "\(formattedPace)"
+        loadMap()
+    }
+    
+    private func loadMap() {
+      guard
+        let locations = run.locations,
+        locations.count > 0,
+        let region = mapRegion()
+      else {
+          let alert = UIAlertController(title: "Error",
+                                        message: "Sorry, this run has no locations saved",
+                                        preferredStyle: .alert)
+          alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+          present(alert, animated: true)
+          return
+      }
         
     }
+    
+    
 
 }
