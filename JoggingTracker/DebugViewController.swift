2a5ebf63cd7aa24c253bde7da966cc1330c2f36d
lucas-goldner <lucas.goldner@googlemail.com>
2020-12-19 18:43:26 +0100

    Basic Websocket Setup

--- JoggingTracker/DebugViewController.swift
+++ JoggingTracker/DebugViewController.swift
@@ -6,13 +6,23 @@
 //
 
 import UIKit
+import Starscream
 
-class DebugViewController: UIViewController {
-
+class DebugViewController: UIViewController, WebSocketDelegate {
+     var socket: WebSocket!
+     var isConnected = false
+     let server = WebSocketServer()
     @IBOutlet weak var OutputView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        OutputView.text = "jo"
+        var request = URLRequest(url: URL(string: "http://localhost:8080")!) //https://localhost:8080
+        request.timeoutInterval = 5
+        socket = WebSocket(request: request)
+        socket.delegate = self
+        socket.connect()
     }
+
+    
 }
 
