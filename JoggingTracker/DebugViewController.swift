ff17502a3e87182fdb83804bcd483349ff1a3468
lucas-goldner <lucas.goldner@googlemail.com>
2020-12-19 18:33:01 +0100

    Integrated cocoa pods + starscream pod for web sockets

--- JoggingTracker/DebugViewController.swift
+++ JoggingTracker/DebugViewController.swift
@@ -12,38 +12,7 @@ class DebugViewController: UIViewController {
     @IBOutlet weak var OutputView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        getApiResponse()
         OutputView.text = "jo"
-        // Do any additional setup after loading the view.
     }
-    
-    struct Demon: Codable {
-        let level: Int
-        let name, id, weakness, strength, imun, absorb, reflect: String
-
-
-        enum CodingKeys: String, CodingKey {
-            case id = "id"
-            case name, level, weakness, strength, imun, absorb, reflect
-        }
-    }
-    
-   
-    
-    func getApiResponse() {
-        let urlString = "http://159.69.196.125:4220/api/demons/1"
-               guard let url = URL(string: urlString) else {return}
-               URLSession.shared.dataTask(with: url) { (data, res, error) in
-           
-                   do {
-                       let demonDetails = try JSONDecoder().decode(Demon.self, from: data!)
-                       DispatchQueue.main.async {
-                 
-                        self.OutputView.text = demonDetails.name
-                       }
-                   } catch {}
-               }.resume()
-           }
-    
-    }
+}
 
