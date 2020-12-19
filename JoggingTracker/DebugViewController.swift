e9cc4668123a454857bec2cfee4a6327779cf135
lucas-goldner <lucas.goldner@googlemail.com>
2020-12-19 18:44:26 +0100

    Added receive function to web socket

--- JoggingTracker/DebugViewController.swift
+++ JoggingTracker/DebugViewController.swift
@@ -23,6 +23,33 @@ class DebugViewController: UIViewController, WebSocketDelegate {
         socket.connect()
     }
 
+    func didReceive(event: WebSocketEvent, client: WebSocket) {
+          switch event {
+          case .connected(let headers):
+              isConnected = true
+              print("websocket is connected: \(headers)")
+          case .disconnected(let reason, let code):
+              isConnected = false
+              print("websocket is disconnected: \(reason) with code: \(code)")
+          case .text(let string):
+              print("Received text: \(string)")
+          case .binary(let data):
+              print("Received data: \(data.count)")
+          case .ping(_):
+              break
+          case .pong(_):
+              break
+          case .viabilityChanged(_):
+              break
+          case .reconnectSuggested(_):
+              break
+          case .cancelled:
+              isConnected = false
+          case .error(let error):
+              isConnected = false
+              handleError(error)
+          }
+      }
     
 }
 
